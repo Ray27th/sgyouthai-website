1384acf81d109ca7434aa93dfe9df183d5a1862a
Ray27th <100133474+27thRay@users.noreply.github.com>
2024-11-20 15:16:00 +0800

    Added PFP and fix padding issues

--- src/pages/LinkInBio.tsx
+++ src/pages/LinkInBio.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import heroBG from "@/assets/hero-bg.png";
+import Logo from "@/assets/logo.png";
 
 export default function LinkInBio() {
   const [showHeader, setShowHeader] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 75) {
         setShowHeader(false);
       } else {
         setShowHeader(true);
@@ -29,8 +30,12 @@ export default function LinkInBio() {
       }}
     >
       <div className={cn("fixed p-5 top-5 w-full flex justify-between transition-all", showHeader ? "opacity-1" : "opacity-0")}>
-       <div>btn1</div>
-       <div>btn2</div>
+        <div>btn1</div>
+        <div>btn2</div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <img src={Logo} className="h-32 w-32 rounded-full" />
+        <h2 className="text-lg font-bold">sgyouthai</h2>
       </div>
       <p>Link In Bio</p>
     </div>
