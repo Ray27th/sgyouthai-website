1d024e5912851b29f463244567a1cebc419e6b01
UltraRaptor <sohhongyu@gmail.com>
2024-11-20 14:28:45 +0800

    Added header

--- src/pages/LinkInBio.tsx
+++ src/pages/LinkInBio.tsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import cn from "@/lib/utils"
+
 export default function LinkInBio() {
-  return <div>Link In Bio</div>
+  const [showHeader, setShowHeader] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShowHeader(false);
+      } else {
+        setShowHeader(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="relative h-[200vh]">
+      <div className={cn("fixed top-5 w-full flex justify-between transition-all", showHeader ? "opacity-1" : "opacity-0")}>
+       <div>btn1</div>
+       <div>btn2</div>
+      </div>
+      <p>Link In Bio</p>
+    </div>
+  );
 }
