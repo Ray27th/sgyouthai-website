4683563872ba62c732755da42051af48ad03cc61
Soh Hong Yu <raymondng009@gmail.com>
2024-11-24 09:24:27 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,26 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ReactGA from "react-ga";
 
-// Initialize Google Analytics
+import { PostHogProvider } from "posthog-js/react";
+
+// Initialise Google Analytics
 const TRACKING_ID = import.meta.env.VITE_GOOGLE_ANALYTICS || "";
 ReactGA.initialize(TRACKING_ID, { debug: import.meta.env.PROD });
 
+// Initialise PostHog
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+};
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_POSTHOG_KEY}
+      options={options}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
   </StrictMode>
 );
