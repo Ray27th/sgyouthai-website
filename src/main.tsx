bd3cde41f4f763517ac3a27a4fcd8f4b9d2000e4
Soh Hong Yu <raymondng009@gmail.com>
2024-11-24 10:19:51 +0800

    Posthog tracking

--- src/main.tsx
+++ src/main.tsx
@@ -5,24 +5,24 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ReactGA from "react-ga";
 
+import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import PostHogPageviewTracker from "@/hooks/use-posthog-pageview-tracking.ts";
 
 // Initialise Google Analytics
 const TRACKING_ID = import.meta.env.VITE_GOOGLE_ANALYTICS || "";
 ReactGA.initialize(TRACKING_ID, { debug: import.meta.env.PROD });
 
 // Initialise PostHog
-const options = {
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
   api_host: import.meta.env.VITE_POSTHOG_HOST,
-};
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_POSTHOG_KEY}
-      options={options}
-    >
+    <PostHogProvider client={posthog}>
       <BrowserRouter>
+        <PostHogPageviewTracker />
         <App />
       </BrowserRouter>
     </PostHogProvider>
