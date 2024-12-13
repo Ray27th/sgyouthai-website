f128a62f4367fc38e7da80f1ed0be970f2422b80
Soh Hong Yu <raymondng009@gmail.com>
2024-12-14 01:59:04 +0800

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -15,7 +15,7 @@ export function PostHogProvider({ children }: PostHogProviderProps) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "",
       person_profiles: "always",
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      capture_pageview: false,
       capture_pageleave: true,
     });
   }, []);
