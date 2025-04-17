adcb5cc23daf0f3c430ab4c2a2cae80d691e53e5
Ray27th <raymondng009@gmail.com>
2025-04-17 22:23:16 +0800

    Create route.ts

---
+++ src/app/events/[slug]/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  context: { params: { slug: string } }
+) {
+  const { slug } = context.params;
+
+  let targetUrl = "/";
+
+  if (slug === "echelon2025") {
+    targetUrl =
+      "https://e27-dot-yamm-track.appspot.com/2wRbZvIkpK8tJ9Gga5xS3a4KoECgSwbJWeMlgC-zTrLP7uyYUlgHyrp4N5_EInwd19REmIL4GplqlWT15wvkk8j-6bzkg_x-SJzZEddEkfj8x76mf3zgcd7O3YgFL6jJu7xhlHzaGfHDM6Tuo-I_agGDCRcEokglkUCuER8l8QpuyYP5HmrxDeUqlIZEgzfBsYnuUsivp3cfe-i8XMe9WRoTWZsc";
+  }
+
+  return NextResponse.redirect(targetUrl);
+}
