5d5a135be94f431d2328000d451b2be006cbacf6
Ray27th <raymondng009@gmail.com>
2025-04-17 21:31:57 +0800

    Update page.tsx

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -1,17 +1,15 @@
-// app/redirect/[slug]/page.tsx
 import { redirect } from "next/navigation";
 
 export default function RedirectPage({ params }: { params: { slug: string } }) {
   const { slug } = params;
 
-  // Example condition logic (you can change this)
   let targetUrl = "/";
 
   if (slug === "echelon2025") {
     targetUrl =
       "https://e27-dot-yamm-track.appspot.com/2wRbZvIkpK8tJ9Gga5xS3a4KoECgSwbJWeMlgC-zTrLP7uyYUlgHyrp4N5_EInwd19REmIL4GplqlWT15wvkk8j-6bzkg_x-SJzZEddEkfj8x76mf3zgcd7O3YgFL6jJu7xhlHzaGfHDM6Tuo-I_agGDCRcEokglkUCuER8l8QpuyYP5HmrxDeUqlIZEgzfBsYnuUsivp3cfe-i8XMe9WRoTWZsc";
   } else {
-    targetUrl = "/"; // Or any fallback
+    targetUrl = "/";
   }
 
   redirect(targetUrl);
